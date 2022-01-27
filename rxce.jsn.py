1f3b1a66bf439ce36033897d6c094203dc1a8b9b
shantanuMD <98452964+shantanuMD@users.noreply.github.com>
2022-01-27 15:09:38 +0530

    Update rxce.jsn.py

--- rxce.jsn.py
+++ rxce.jsn.py
@@ -1,2 +1,136 @@
-rxce next colour prediction
-print ("red colour")
+
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+
+    
+            for c in itertools.cycle(['|', '/', '-', '\\']) :
+                if done:
+                    break
+                sys.stdout.write('\rhacking in the parity server for next colour--------- ' + c)
+                sys.stdout.flush()
+                time.sleep(0.1)
+            sys.stdout.write('\rDone!     ')
+
+        t = threading.Thread(target=animate)
+        t.start()
+
+        #long process here
+        time.sleep(20)
+        done = True
+
+    
+        
+            for c in itertools.cycle(['|', '/', '-', '\\']):
+                if done:
+                    break
+                sys.stdout.write('\rgetting the colour wait --------- ' + c)
+                sys.stdout.flush()
+                time.sleep(0.1)
+            sys.stdout.write('\rDone!     ')
+
+        t = threading.Thread(target=animate)
+        t.start()
+
+        #long process here
+        time.sleep(20)
+        done = True
+
+    def clear():
+        # for windows
+        if name == 'nt':
+            _ = system('cls')
+        # for mac and linux(here, os.name is 'posix')
+        else:
+            _ = system('clear')
+
+    clear()
+    y=1
+    newperiod=period
+    banner='figlet AMUSEBOX'
+    m=0
+    i=1
+    thisway=[1,2,4,6,7,8,15,14,16,17,18]
+    thatway=[3,5,9,10,11,12,13,19,20]
+    numbers=[]
+    while(y):
+        clear()
+        system(banner)
+        print("Contact me on telegram @shantanu")
+        print("Enter ",newperiod," Parity Price :")
+        current=input()
+        current=int(current)
+        #chalo()
+        print("\n---------Successfully hacked the server-----------")
+        #chalo1()
+        print("\n---------Successfully got the colour -------------")
+        print('\n')
+        def getSum(n):
+            sum=0
+            for digit in str(n):
+                sum += int(digit)
+            return sum
+        if i in thisway:
+            m=getSum(current)
+            n=int(current)%10
+            if((m%2==0 and n%2==0) or (m%2==1 and n%2==1)):
+                if current in numbers:
+                    print(newperiod+1," : RED")
+                else:
+                    print(newperiod+1," : GREEN")
+            else:
+                if current in numbers:
+                    print(newperiod+1," : GREEN")
+                else:
+                    print(newperiod+1," : RED")
+        if i in thatway:
+            m=getSum(current)+1
+            n=int(current)%10
+            if((m%2==0 and n%2==0) or (m%2==1 and n%2==1)):
+                if current in numbers:
+                    print(newperiod+1,": RED")
+                else:
+                    print(newperiod+1,": GREEN")
+            else:
+                if current in numbers:
+                    print(newperiod+1,": GREEN")
+                else:
+                    print(newperiod+1,": RED")
+        i=i+1
+        newperiod+=1
+        numbers.append(current)
+        y=input("Do you want to play : Press 1 and 0 to exit \n")
+        if(y==0):
+            y=False
+        if (len(numbers)>21):
+            clear()
+            system('figlet Thank you!!')
+            print("Play on next specified time!!")
+            print("-----------Current Time UP----------")
+            sys.exit(" \n \n \n Contact on Telegram @shantanu")
+            #print(numbers)
+
+
+=0)
+
+    if (True):
+            period=385
+            hero()
+    elif(False):
+            period=342
+            hero()
+    elif(False):
+            period=343
+            hero()
+    elif(now>Final and now<Finalend):
+            period=400
+            hero()
+    
+        print("Hi!! Thanks for playing")
+        print("----------Your play time-----------")
+        print("Play today" )
+        print("08:00 PM- 08:30 PM",)
+        
+
+
+
